5bc22c40a596f4ac0a920a43b4bedb906ea6f201
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-07-15 03:53:04 +0100

    select option onchange function did set up

--- script.js
+++ script.js
@@ -5,33 +5,58 @@ var skills = $(".skills");
 var interests = $(".Interests")
 var info = $(".info");
 
-$(".me").on("click",function(){  
+$(".me").on("click", function () {
     $(".education, .skills, .experience, .Interests").hide();
-    mySelf.show();  
-    
+    mySelf.show();
+
 });
 
 
-$(".edu").on("click",function(){   
+$(".edu").on("click", function () {
     $(".mySelf, .skills, .experience, .Interests").hide();
-    education.show();  
-    
+    education.show();
+
 });
 
-$(".exp").on("click",function(){   
+$(".exp").on("click", function () {
     $(".mySelf, .skills, .education, .Interests").hide();
-    experience.show();  
-    
+    experience.show();
+
 });
-$(".ski").on("click",function(){ 
-    $(".mySelf, .education, .experience, .Interests").hide(); 
-    skills.show();  
-    
+$(".ski").on("click", function () {
+    $(".mySelf, .education, .experience, .Interests").hide();
+    skills.show();
+
 });
-$(".inter").on("click",function(){   
+$(".inter").on("click", function () {
     $(".mySelf, .skills, .experience, .education").hide();
-    interests.show(); 
-    
+    interests.show();
+
 });
 
-    
+
+$(document).ready(function(){
+    $('.getgrab, .employeetracker').hide();
+    $('.bookworm').show();
+});
+function myProject() {
+    const project = $('#projectname').val()
+    console.log(project);
+    if (project === 'getgrab') {
+        $('.bookworm, .employeetracker').hide();
+        $('.getgrab').show();
+    }
+    if (project === 'employeetracker') {
+        $('.getgrab, .bookworm').hide();
+        $('.employeetracker').show();
+    }
+    if (project === 'bookworm') {
+        $('.getgrab, .employeetracker').hide();
+        $('.bookworm').show();
+    }
+};
+
+$(document).ready(function(){
+    $('select').formSelect();
+  });
+
